0edee8b78fca6d162c8770e1a9519195a9d9a5aa
kevinbaddata <kevinduzguner.05@gmail.com>
2020-12-30 19:33:30 +0100

    Added more layers to the hair page

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,3 +26,37 @@ $(".slider .owl-carousel").owlCarousel({
     },
   },
 });
+
+$(".slider-hair .owl-carousel").owlCarousel({
+  autoplay: true,
+  lazyLoad: true,
+  rewind: true,
+  responsiveClass: true,
+  autoHeight: true,
+  autoplayTimeout: 4000,
+  smartSpeed: 735,
+  dots: false,
+  maxHeight: 80,
+  responsive: {
+    0: {
+      items: 1,
+    },
+
+    600: {
+      items: 2,
+    },
+
+    1024: {
+      items: 3,
+    },
+
+    1366: {
+      items: 4,
+    },
+  },
+});
+
+
+
+
+
